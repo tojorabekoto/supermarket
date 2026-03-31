f85ece33c7c03352c5a7858442d74f190d444189
tojorabekoto <tojo.rabekoto.0@gmail.com>
2026-03-31 11:40:52 +0300

    insert base

--- bdd/data.sql
+++ bdd/data.sql
@@ -23,3 +23,14 @@ create table achat (
     foreign key (product_id) references product(id),
     foreign key (caisse_id) references caisse(id)
 );
+
+insert into product (name, price, quantitystock) values
+('Pomme', 0.50, 100),
+('Banane', 0.30, 150),
+('Orange', 0.40, 120),
+('Lait', 1.20, 80),
+('Pain', 1.00, 50);
+
+insert into caisse (name) values
+('Caisse 1'),
+('Caisse 2');
